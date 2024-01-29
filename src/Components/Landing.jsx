aa0a82f69ed695b287713978e74a5caf8af2244e
Manish <i.m.manish100202@gmail.com>
2024-01-29 22:39:37 +0530

    Same

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = ({toggle}) => {
     <div className=" pt-11  lg:flex  md:flex md:justify-center lg:justify-center sm:justify-center sm:items-center sm:flex-wrap items-center">
         <img src="./image/dices.png" className="lg:w-[640px] md:w-[300px] sm:flex-wrap sm:text-center sm:w-[300px]" alt="" />
         <div>
-          <h1 className=" text-center text-5xl  sm:items-center sm:text-center font-extrabold lg:text-[7vw] ">Manish Game</h1>
+          <h1 className=" text-center text-5xl  sm:items-center sm:text-center font-extrabold lg:text-[7vw] ">DICE GAME</h1>
           <div  className=" text-center md:text-right lg:text-right md:text-right sm:text-center "> <Buttons onClick={toggle} text="Play Now"/> </div>
         </div>
     </div>
